eed44eba6033ab4b514d01171c80b93d9ff709a6
densPro <densPro09.07.95@gmail.com>
2015-07-05 18:09:14 -0400

    agenda telefonica y IU de la calculadora

--- Calculate/app/src/main/java/com/example/denis/calculate/MainActivity.java
+++ Calculate/app/src/main/java/com/example/denis/calculate/MainActivity.java
@@ -1,12 +1,9 @@
-package com.example.denis.people;
+package com.example.denis.calculate;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -15,14 +12,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       Button btnAddContact = (Button)findViewById(R.id.btnAdd);
-        btnAddContact.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),Contact.class);
-                startActivity(i);
-            }
-        });
     }
 
     @Override
